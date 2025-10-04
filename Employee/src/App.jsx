25763e6dcd4d74597e8d53411be9162bd7784f9b
DrashtiMistry <drashtimistry2111@gmail.com>
2025-10-04 12:21:29 +0530

    Form added

--- Employee/src/App.jsx
+++ Employee/src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
-
+import HomePage from './pages/HomePage'
 
 function App() {
   
 
   return (
     <>
-     <h1 className="text-4xl font-bold">Hello Tech Titans</h1>
+     {/* <h1 className="text-4xl font-bold">Hello Tech Titans</h1> */}
+     <HomePage/>
     </>
   )
 }
